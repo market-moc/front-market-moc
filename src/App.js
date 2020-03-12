e8c77ef541423d2c4dfd66f9bb751bb5c48191b7
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2020-03-12 14:51:27 +0100

    First transactions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Tabs from './Components/Tabs';
+import Transactions from './Components/Transactions';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Tabs />
+      <Switch>
+        <Route path="/buy">
+          <Transactions />
+        </Route>
+        <Route path="/sell">
+          <Transactions />
+        </Route>
+        <Route path="/">
+          <Transactions />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
